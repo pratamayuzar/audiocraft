9b07ff3856bdf75b06d67a213c84380f8aded656
Yuzar Pratama <yuzar.pratama@peeba.com>
2025-02-18 08:05:03 +0700

    Feat: change latest checkpoint

--- inference/export_model.py
+++ inference/export_model.py
@@ -2,7 +2,7 @@ import os
 from audiocraft.utils import export
 
 # Define paths
-CHECKPOINT_PATH = "/workspace/trained_model/vocal/xps/e1cd28f5/checkpoint.th"
+CHECKPOINT_PATH = "/workspace/trained_model/no-vocal/xps/97868b63/checkpoint.th"
 EXPORT_DIR = "/workspace/audiocraft/checkpoints/my_audio_lm_no_vocal/"
 ENCODEC_MODEL = "facebook/encodec_32khz"  # Change if using a custom EnCodec model
 
