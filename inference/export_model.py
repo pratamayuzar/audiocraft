9650f69b4a3ef5ec4aab873fb93db953c29441ad
Yuzar Pratama <yuzar.pratama@peeba.com>
2025-02-28 06:39:44 +0700

    feat: change new train

--- inference/export_model.py
+++ inference/export_model.py
@@ -2,8 +2,8 @@ import os
 from audiocraft.utils import export
 
 # Define paths
-CHECKPOINT_PATH = "/workspace/trained_model/no_vocal_250226/xps/14ba9f91/checkpoint.th"
-EXPORT_DIR = "/workspace/audiocraft/checkpoints/my_audio_lm_no_vocal_250226/"
+CHECKPOINT_PATH = "/workspace/trained_model/no_vocal_250228/xps/14ba9f91/checkpoint.th"
+EXPORT_DIR = "/workspace/audiocraft/checkpoints/my_audio_lm_no_vocal_250228/"
 ENCODEC_MODEL = "facebook/encodec_32khz"  # Change if using a custom EnCodec model
 
 # Ensure export directory exists
