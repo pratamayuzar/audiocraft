ccd6427668990b32e79f0159786ea369c1a23b38
Yuzar Pratama <yuzar.pratama@peeba.com>
2025-02-28 11:44:00 +0700

    feat: change prompt

--- inference/inference.py
+++ inference/inference.py
@@ -13,22 +13,22 @@ magnet = audiocraft.models.MAGNeT.get_pretrained(CHECKPOINT_DIR)
 print("✅ Model loaded successfully!")
 
 # Define text prompt for music generation
-prompt = "Energetic upbeat koplo music with strong kendang and bass, suitable for dancing."
-# prompt = "A lively koplo track with driving kendang rhythms, cheerful melodies on the keyboard, and strong, punchy percussion"
+# prompt = "Energetic upbeat koplo music with strong kendang and bass, suitable for dancing."
+prompt = "A lively koplo track with driving kendang rhythms, cheerful melodies on the keyboard, and strong, punchy percussion"
 
 # Generate audio (MAGNeT will handle device placement)
 # Set maximum duration (e.g., 90 seconds)
 magnet.max_duration = 90  # Change this to your desired duration
 
 # Generate music
-print("🎵 Generating music for", magnet.max_duration, "seconds...")
+print("🎵 Generating music for", magnet.max_duration, "seconds...", prompt)
 output = magnet.generate([prompt], progress=True)  # Ensure the prompt is wrapped in a list
 
 # Move generated tensor to CPU before saving
 audio_tensor = output[0].cpu()
 
 # Save audio file
-OUTPUT_PATH = "/workspace/audiocraft/inference/generated_music_no_vocal_250228.wav"
+OUTPUT_PATH = "/workspace/audiocraft/inference/generated_music_no_vocal_250228_lively.wav"
 torchaudio.save(OUTPUT_PATH, audio_tensor, sample_rate=32000)
 
 print(f"✅ Audio file saved at: {OUTPUT_PATH}")
