959c0ca53b853894860883eaf90bc3bd25fdd659
Yuzar Pratama <yuzar.pratama@peeba.com>
2025-03-23 11:16:30 +0700

    feat: add 5 prompt

--- inference_musicgen/inference.py
+++ inference_musicgen/inference.py
@@ -12,7 +12,10 @@ print("✅ Model loaded successfully!")
 # Define multiple text prompts
 prompts = [
     "Energetic upbeat koplo music with strong kendang and bass, suitable for dancing.",
-    "A lively koplo track with driving kendang rhythms, cheerful melodies on the keyboard, and strong, punchy percussion."
+    "A lively koplo track with driving kendang rhythms, cheerful melodies on the keyboard, and strong, punchy percussion.",
+    "A dynamic Koplo composition blending traditional elements with electronic beats, featuring sharp kendang accents, deep bass, and vibrant brass for an exciting fusion sound.",
+    "A mid-tempo, smooth Koplo tune featuring soft kendang rhythms, melodic flute sections, and warm keyboard harmonies, evoking a relaxed yet emotional mood.",
+    "A laid-back, groove-driven Koplo song with deep, rolling kendang, soulful electric guitar licks, and smooth synth pads, perfect for a chilled yet rhythmic listening experience.",
 ]
 
 # Set custom duration (e.g., 60 seconds)
@@ -23,7 +26,7 @@ print(f"🎵 Generating {musicgen.duration}-second music...")
 output = musicgen.generate(prompts, progress=True)  # No duration argument here
 
 # Save generated audio files
-OUTPUT_DIR = "/workspace/audiocraft/inference_musicgen/audio-250319/"
+OUTPUT_DIR = "/workspace/audiocraft/inference_musicgen/audio-250319/new/"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 for i, audio_tensor in enumerate(output):
